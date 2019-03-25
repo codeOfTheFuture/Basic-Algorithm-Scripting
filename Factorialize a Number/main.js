eac1e77ee1267884ff7fe62207b6fdcaa5acb67f
Jeff Oliver <jeffoliver3384@gmail.com>
2019-03-25 17:24:12 -0400

    Refactored first solution add second solution using ternary operator and recursion

--- Factorialize a Number/main.js
+++ Factorialize a Number/main.js
@@ -1,15 +1,23 @@
-function factorialize(num) {
-  if(num === 0) {
-    return 1;
-  } else {
-     const intArray = [];
-     for(let i = 1; i <= num; i++) {
-       intArray.push(i);
-     }
-     const multiplyInts = (total, value) => total * value;
-     const factorial = intArray.reduce(multiplyInts);
+// function factorialize(num) {
+//   if(num === 0) {
+//     return 1;
+//   } else {
+//      const numArr = [];
+//      for(let i = 1; i <= num; i++) {
+//        numArr.push(i);
+//      }
+//      const result = numArr.reduce((prev, curr) => {
+//       return prev * curr;
+//      });
  
-     return factorial;
-   }
- }
- factorialize(5);
+//      return result;
+//    }
+//  }
+//  console.log(factorialize(5));
+
+function factorialize(num) {
+  return num === 0 ? 1 :
+         num * factorialize(num -1);
+}
+
+console.log(factorialize(5));
